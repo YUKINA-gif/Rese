f38fde80cc8c71d8af92123abb2424adcb516f7d
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-07-09 17:13:44 +0900

    会員登録バリデーション追加

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,73 +3,93 @@
     <!-- 会員登録 -->
     <h2>新規会員登録</h2>
     <div class="register_card">
-      <form>
-        <ul>
-          <li>
-            <!-- 名前 -->
-            <label for="name">
-              <font-awesome-icon icon="user" class="icon"
-            /></label>
-            <input
-              type="text"
-              id="name"
-              placeholder="お名前"
-              v-model="name"
-            /><br />
-            <!-- 名前エラーメッセージ -->
-            <div class="error" v-if="name_required">
-              お名前が入力されていません
-            </div>
-          </li>
-          <li>
-            <!-- メールアドレス -->
-            <label for="email">
-              <font-awesome-icon icon="envelope" class="icon"
-            /></label>
-            <input
-              type="email"
-              id="email"
-              placeholder="メールアドレス"
-              v-model="email"
-            />
-            <!-- メールアドレスエラーメッセージ -->
-            <div class="error" v-if="email_required">
-              メールアドレスが入力されていません
-            </div>
-            <div class="error" v-if="not_email">
-              メールアドレスで入力してください
-            </div>
-          </li>
-          <li>
-            <!-- パスワード -->
-            <label for="password">
-              <font-awesome-icon icon="key" class="icon"
-            /></label>
-            <input
-              type="password"
-              id="password"
-              placeholder="パスワード"
-              v-model="password"
-            />
-            <!-- パスワードエラーメッセージ -->
-            <div class="error" v-if="password_required">
-              パスワードが入力されていません
-            </div>
-            <div class="error" v-if="password_min_length">
-              8文字以上で入力してください
-            </div>
-          </li>
-        </ul>
-        <!-- 会員登録ボタン -->
-        <button @click="register" type="button" class="button">
-          新規会員登録
-        </button>
-      </form>
+      <ValidationObserver ref="observer">
+        <form>
+          <ul>
+            <li>
+              <!-- 名前 -->
+              <ValidationProvider
+                rules="required"
+                ref="myname"
+                v-slot="{ errors }"
+              >
+                <label for="name">
+                  <font-awesome-icon icon="user" class="icon"
+                /></label>
+                <input
+                  type="text"
+                  id="name"
+                  name="お名前"
+                  placeholder="お名前"
+                  v-model="name"
+                />
+                <span class="error">{{ errors[0] }}</span>
+              </ValidationProvider>
+            </li>
+            <li>
+              <ValidationProvider
+                rules="required|email"
+                ref="mypass"
+                v-slot="{ errors }"
+              >
+                <!-- メールアドレス -->
+                <label for="email">
+                  <font-awesome-icon icon="envelope" class="icon"
+                /></label>
+                <input
+                  type="email"
+                  id="email"
+                  name="メールアドレス"
+                  placeholder="メールアドレス"
+                  v-model="email"
+                />
+                <span class="error">{{ errors[0] }}</span>
+              </ValidationProvider>
+            </li>
+            <li>
+              <ValidationProvider
+                rules="required|min:8"
+                ref="mypass"
+                v-slot="{ errors }"
+              >
+                <!-- パスワード -->
+                <label for="password">
+                  <font-awesome-icon icon="key" class="icon"
+                /></label>
+                <input
+                  type="password"
+                  id="password"
+                  name="パスワード"
+                  placeholder="パスワード"
+                  v-model="password"
+                />
+                <span class="error">{{ errors[0] }}</span>
+              </ValidationProvider>
+            </li>
+          </ul>
+          <!-- 会員登録ボタン -->
+          <button @click="register" type="button" class="button">
+            新規会員登録
+          </button>
+        </form>
+      </ValidationObserver>
     </div>
   </div>
 </template>
 
 <script>
+import {
+  ValidationObserver,
+  ValidationProvider,
+  extend,
+  localize,
+} from "vee-validate";
+import ja from "vee-validate/dist/locale/ja";
+import * as rules from "vee-validate/dist/rules";
+for (let rule in rules) {
+  extend(rule, rules[rule]);
+}
+localize("ja", ja);
 import axios from "axios";
 export default {
   data() {
@@ -85,45 +105,37 @@ export default {
       password_min_length: false,
     };
   },
+  components: {
+    ValidationObserver,
+    ValidationProvider,
+  },
   methods: {
     // 会員登録
     register() {
       // バリデーション
-      // 名前が空ならメッセージ表示
-      if (this.name == "") {
-        this.name_required = true;
-        // メールアドレスが空ならメッセージ表示
-      }
-      if (this.email == "") {
-        this.email_required = true;
-        // パスワードが空ならメッセージ表示
-      }
-      if (this.password == "") {
-        this.password_required = true;
-        // パスワードが8文字以下ならメッセージ表示
-      }
-      if (this.password.length < 7) {
-        this.password_min_length = true;
-        // メールアドレスでなければメッセージ表示
-      }
-      if (!this.email.includes("@")) {
-        this.not_email = true;
-      } else {
-        //上記以外なら会員登録
-        axios
-          .post("https://rese-booking.herokuapp.com/api/user", {
-            name: this.name,
-            email: this.email,
-            password: this.password,
-          })
-          .then((response) => {
-            console.log(response);
-            this.$router.replace("/thanks");
-          })
-          .catch(() => {
-            alert("会員登録ができませんでした。お手数ですが再度お試しください");
-          });
-      }
+      this.$refs.observer.validate().then((res) => {
+        // バリデーションが有効ならスルー
+        if (res == false) {
+          console.log(res);
+          // そうでなければ登録処理
+        } else {
+          axios
+            .post("https://rese-booking.herokuapp.com/api/user", {
+              name: this.name,
+              email: this.email,
+              password: this.password,
+            })
+            .then((response) => {
+              console.log(response);
+              this.$router.replace("/thanks");
+            })
+            .catch(() => {
+              alert(
+                "会員登録ができませんでした。お手数ですが再度お試しください"
+              );
+            });
+        }
+      });
     },
   },
 };
@@ -170,9 +182,6 @@ export default {
   .error,
   span {
     color: red;
-  }
-  .error {
-    margin-left: 30%;
   }
   .icon {
     width: 28px;
